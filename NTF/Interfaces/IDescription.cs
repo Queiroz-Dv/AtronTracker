865f64f6dfcc759cc1c4c19044a1f670bcf58c8b
Queiroz <kalifor_dbel@hotmail.com>
2023-05-31 08:45:50 -0300

    Implementando a framework de notificações

---
+++ NTF/Interfaces/IDescription.cs
@@ -0,0 +1,20 @@
+﻿namespace NTF.Interfaces
+{
+    /// <summary>
+    /// Interface que define uma descrição utilizada em notificações. <br></br>
+    /// Fornece a propriedade de mensagem e o método de representação em string.
+    /// </summary>
+    public interface IDescription
+    {
+        /// <summary>
+        /// Obtém a mensagem da descrição.
+        /// </summary>
+        string Message { get; }
+
+        /// <summary>
+        /// Retorna a representação em string da descrição.
+        /// </summary>
+        /// <returns>A mensagem da descrição.</returns>
+        string ToString();
+    }
+}
