e63c8829dcd51e29276c08b7bc61fe79d5a382de
Queiroz <kalifor_dbel@hotmail.com>
2023-06-11 21:47:50 -0300

    Adicionando form padrão e configurações para a nova estrutura

--- PersonalTracking/Program.cs
+++ PersonalTracking/Program.cs
@@ -13,7 +13,8 @@ namespace PersonalTracking
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmLogin());
+            //Application.Run(new FrmLogin());
+            Application.Run(new FrmMainMenuView());
         }
     }
 }
