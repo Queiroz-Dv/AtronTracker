49d5ab7b515ec8d36581218f4be3029b35823a37
Queiroz <kalifor_dbel@hotmail.com>
2023-06-18 21:52:46 -0300

    Adicionando projeto de API e projeto de Serviços.

---
+++ SRV/ServiceContainer.cs
@@ -0,0 +1,27 @@
+﻿using BLL.Interfaces;
+using BLL.Services;
+using DAL;
+using DAL.DAO;
+using DAL.Interfaces;
+using DAL.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SRV
+{
+    public class ServiceContainer
+    {
+        public static IServiceCollection ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Registre os serviços aqui
+            services.AddScoped<IEmployeeRepository<EMPLOYEE>, EmployeeRepository>();
+            services.AddScoped<IEmployeeService<EMPLOYEE>, EmployeeService>();
+            services.AddScoped<IDepartmentRepository<DEPARTMENT>, DepartmentRepository>();
+            services.AddScoped<IPositionRepository<POSITION>, PositionRepository>();
+            services.AddScoped<Context>();
+
+            return services;
+        }
+    }
+}
