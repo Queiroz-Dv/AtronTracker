b001a82202a7cad9170e68252a0b78191e74ab98
Queiroz <kalifor_dbel@hotmail.com>
2024-07-29 23:25:35 -0300

    Estrutura para os processos de Cargo

---
+++ Framework/ExternalServices/Interfaces/ICargoExternalService.cs
@@ -0,0 +1,9 @@
+﻿using Atron.Application.DTO;
+
+namespace ExternalServices.Interfaces
+{
+    public interface ICargoExternalService
+    {
+        Task<List<CargoDTO>> ObterTodos();
+    }
+}
---
+++ Framework/ExternalServices/Services/CargoExternalService.cs
@@ -0,0 +1,34 @@
+﻿using Atron.Application.DTO;
+using Communication.Interfaces.Services;
+using Communication.Interfaces;
+using ExternalServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ExternalServices.Services
+{
+    public class CargoExternalService : ICargoExternalService
+    {
+        private readonly IApiClient _apiClient;
+        private readonly ICommunicationService _communicationService;
+
+        public CargoExternalService(IApiClient apiClient, ICommunicationService communicationService)
+        {
+            _apiClient = apiClient;
+            _communicationService = communicationService;
+        }
+
+        public async Task<List<CargoDTO>> ObterTodos()
+        {
+            var response = await _apiClient.GetAsync("https://atron-hmg.azurewebsites.net/api/Cargo/ObterCargos");
+
+            var cargos = JsonConvert.DeserializeObject<List<CargoDTO>>(response);
+
+            return cargos;
+        }
+    }
+}
