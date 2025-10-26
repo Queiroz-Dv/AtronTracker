700add3a4a277471af377e33a2782ba1cf87996a
Eduardo Queiroz <queiroz.dv@outlook.com>
2025-10-26 16:43:44 -0300

    FEAT: Estrutura inicial do módulo de estoque preparada para migração

--- AtronTracker/Infrastructure/Context/AtronDbContext.cs
+++ AtronTracker/Infrastructure/Context/AtronDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Domain.Componentes;
-using Domain.Entities;
+﻿using Domain.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Shared.Models.ApplicationModels;
@@ -39,23 +38,7 @@ namespace Infrastructure.Context
         public DbSet<PerfilDeAcessoUsuario> PerfilDeAcessoUsuarios { get; set; }
 
         public DbSet<PerfilDeAcessoModulo> PerfilDeAcessoModulos { get; set; }
-
-        public DbSet<PropriedadesDeFluxo> PropriedadesDeFluxo { get; set; }
-
-        public DbSet<PropriedadeDeFluxoModulo> PropriedadeDeFluxoModulo { get; set; }
-
-        public DbSet<Cliente> Clientes { get; set; }
-
-        public DbSet<Categoria> Categorias { get; set; }
-
-        public DbSet<Produto> Produtos { get; set; }
-
-        public DbSet<Venda> Vendas { get; set; }
-
-        public DbSet<Auditoria> Auditorias { get; set; }
-
-        public DbSet<Historico> Historicos { get; set; }
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
